23ba964256288f05d9b59aeb1cee0985d9342fe6
Adisht Jaglan <adishtjaglan@Adishts-MacBook-Air.local>
2024-02-03 19:59:01 +0530

    add CRUD routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./model/campground");
 
 const app = express();
@@ -19,6 +20,10 @@ mongoose.connect(`mongodb://127.0.0.1:27017/${dbHost}`)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
     console.log("Working!");
     res.render("home");
@@ -31,11 +36,49 @@ app.get("/campground", async (req, res) => {
     res.render("campgrounds/index", { campgrounds });
 });
 
+//make a new campground
+app.get("/campground/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+//create a new product
+app.post("/campground", async (req, res) => {
+    const newCampground = new Campground(req.body);
+    await newCampground.save();
+
+    res.redirect(`/campground/${newCampground._id}`);
+});
+
+//view a campground
 app.get("/campground/:id", async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
 
     res.render("campgrounds/show", { campground });
+});
+
+//update campground
+app.get("/campground/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const findCampground = await Campground.findById(id);
+
+    res.render("campgrounds/edit", { findCampground });
+});
+
+//display updated campground
+app.put("/campground/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+
+    res.redirect(`/campground/${campground._id}`);
+});
+
+//deleting a campground
+app.delete("/campground/:id", async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+
+    res.redirect("/campground");
 });
 
 app.listen(3000, () => {
